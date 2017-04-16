258055fe45da6c99b77a81fa78b70f3d4d170e98
unai.esteibar <unai.esteibar@xing.com>
2017-04-16 08:45:12 +0200

    feat(window): Add fallback to duckduckgo search

--- src/helpers/urls.js
+++ src/helpers/urls.js
@@ -1,10 +1,14 @@
 import { urls } from '../constants'
 
 class Urls {
-  url = (command) => {
+  url = command => {
     const cleanCommand = command.replace(/[^0-9a-z]/gi, '')
-    return urls[cleanCommand]
-  }
+    return urls[cleanCommand] || this._fallback(cleanCommand)
+  };
+
+  _fallback = command => {
+    return `https://duckduckgo.com/?q=${command}+documentation&t=h_&ia=web`
+  };
 }
 
-export default (new Urls)
+export default new Urls()
